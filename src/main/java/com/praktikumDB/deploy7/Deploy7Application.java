f7c90223a9629d4d99d8e54693decdb557b3ab7b
M S I <M S I@MSI>
2026-04-20 14:01:16 +0700

    properties

---
+++ src/main/java/com/praktikumDB/deploy7/Deploy7Application.java
@@ -0,0 +1,13 @@
+package com.praktikumDB.deploy7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikumDB/deploy7/Deploy7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikumDB.deploy7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
